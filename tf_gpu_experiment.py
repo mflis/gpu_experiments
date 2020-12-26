8c6d703bf445859eaf16e3712c8b7e7c60e5c1a5
Marcin Flis <marcinf@qualtrics.com>
2020-12-26 21:51:23 +0100

    preprocessing works

--- tf_gpu_experiment.py
+++ tf_gpu_experiment.py
@@ -1,32 +1,32 @@
 from tensorflow.python.compiler.mlcompute import mlcompute
-# mlcompute.set_mlc_device(device_name='gpu')
 from datasets import load_dataset
 from transformers import TFAutoModelForSequenceClassification, AutoTokenizer
 import tensorflow as tf
 
-# model_name = "bert-base-uncased"
-model_name = "albert-base-v2"
+mlcompute.set_mlc_device(device_name='gpu')
+
+
+def prepare_dataset(split: str):
+    # processing code based on https://huggingface.co/docs/datasets/torch_tensorflow.html
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+    dataset = load_dataset('glue', 'mrpc', split=split)
+    dataset = dataset.map(lambda x: tokenizer(x['sentence1'], x['sentence2'], truncation=True, padding='max_length'),
+                          batched=True)
+    dataset = dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+
+    dataset.set_format(type='tensorflow', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
+    features = {x: dataset[x].to_tensor(shape=[None, tokenizer.model_max_length]) for x in
+                ['input_ids', 'token_type_ids', 'attention_mask']}
+    tfdataset = tf.data.Dataset.from_tensor_slices((features, dataset["labels"])).batch(8)
+    return tfdataset
+
+
+model_name = "bert-base-uncased"
 model = TFAutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-dataset = load_dataset('glue', 'mrpc', split='train')
-
-
-# todo  try reformer model
-# cut on nr of examples, truncation, batch size
-
-def encode(examples):
-    return tokenizer(examples['sentence1'], examples['sentence2'], truncation=True, padding='max_length')
-
-
-dataset = dataset.map(encode, batched=True)
-dataset = dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
-
-dataset.set_format(type='tensorflow', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
-features = {x: dataset[x].to_tensor(default_value=0, shape=[None, tokenizer.model_max_length]) for x in
-            ['input_ids', 'token_type_ids', 'attention_mask']}
-tfdataset = tf.data.Dataset.from_tensor_slices((features, dataset["labels"])).batch(1)
+tfds_train_dataset = prepare_dataset('train')
+tfds_test_dataset = prepare_dataset('test')
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE, from_logits=True)
 opt = tf.keras.optimizers.Adam(learning_rate=3e-5)
 model.compile(optimizer=opt, loss=loss_fn, metrics=["accuracy"])
-model.fit(tfdataset, epochs=3)
+model.fit(tfds_train_dataset, epochs=1)
